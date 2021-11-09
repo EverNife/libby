98db4d1d10f6bd372f9673313377388ef7d1cbba
Petrus <petrus_ant@hotmail.com>
2021-11-09 16:52:49 -0300

    Add way to load several libraries at once using a multithread executor

--- core/src/main/java/net/byteflux/libby/LibraryManager.java
+++ core/src/main/java/net/byteflux/libby/LibraryManager.java
@@ -27,6 +27,10 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import static java.util.Objects.requireNonNull;
 
@@ -399,5 +403,39 @@ public abstract class LibraryManager {
         }
 
         addToClasspath(file);
+    }
+
+    /**
+     * Loads a set os libraries jar into the plugin's classpath. If the
+     * library jar doesn't exist locally, it will be downloaded.
+     * <p>
+     * If the provided library has any relocations, they will be applied to
+     * create a relocated jar and the relocated jar will be loaded instead.
+     *
+     * @param libraries the set of libraries to load
+     * @see #downloadLibrary(Library)
+     */
+    public void loadLibraries(Set<Library> libraries) {
+        CountDownLatch latch = new CountDownLatch(libraries.size());
+
+        final ThreadPoolExecutor scheduler = (ThreadPoolExecutor) Executors.newFixedThreadPool(libraries.size() >= 4 ? 4 : libraries.size());
+
+        for (Library library : libraries) {
+            scheduler.submit(() -> {
+                try {
+                    loadLibrary(library);
+                } catch (Throwable e) {
+                    logger.error("Unable to load dependency " + library.toString() + ".", e);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
